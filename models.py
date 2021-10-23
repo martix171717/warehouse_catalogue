3aa24ff7e94cf9e8e520ad47c682bdc37143cbb6
Martyna Szymańska <martix171717@gmail.com>
2021-10-23 13:37:02 +0200

    Add models.py

---
+++ models.py
@@ -0,0 +1,7 @@
+class Product:
+    def __init__(self, name, unit, unit_price, quantity):
+        self.name=name
+        self.unit=unit
+        self.unit_price = unit_price
+        self.quantity = quantity
+
